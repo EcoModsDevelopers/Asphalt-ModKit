25c479a9b874c599bf95e31091a00d82cb45cc85
Kronnox <28481491+Kronnox@users.noreply.github.com>
2018-03-25 18:55:24 +0200

    add: event caller for testing

---
+++ Objects/WoodSignObject.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Strange Loop Games. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace Eco.Mods.TechTree
+{
+    using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Interactions;
+    using Eco.Shared.Serialization;
+    using Gameplay.Components;
+    using Gameplay.Components.Auth;
+
+    [RequireComponent(typeof(CustomTextComponent))]
+    public partial class WoodSignObject : WorldObject
+    {
+        public override InteractResult OnActRight(InteractionContext context)
+        {
+            Eco.Mods.Kronox.AdvancedTeleportation.CallWarpSign(context.Player, GetComponent<CustomTextComponent>().Text);
+
+            Asphalt.api.AsphaltPlugin.Instance.GetEventService().CallEvent(new Asphalt.api.Event.player.PlayerInteractObjectEvent(context));
+
+            return base.OnActRight(context);
+        }
+    }
+
+    [Serialized]
+    public partial class SmallWoodSignObject : WoodSignObject
+    { }
+}
