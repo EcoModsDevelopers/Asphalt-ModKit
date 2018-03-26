fb9a328b1181d4f4d0e405701bbfb5fcd381f2b3
Kronnox <28481491+Kronnox@users.noreply.github.com>
2018-03-26 19:03:52 +0200

    some changes in the project structure / EventManager is now a Signleton

--- Objects/WoodSignObject.cs
+++ Objects/WoodSignObject.cs
@@ -1,23 +1,22 @@
 ﻿// Copyright (c) Strange Loop Games. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using Eco.Gameplay.Objects;
+using Eco.Gameplay.Interactions;
+using Eco.Shared.Serialization;
+using Eco.Gameplay.Components;
+using Asphalt.api.Event.player;
+using Asphalt.api.Event;
+
 namespace Eco.Mods.TechTree
 {
-    using Eco.Gameplay.Objects;
-    using Eco.Gameplay.Interactions;
-    using Eco.Shared.Serialization;
-    using Gameplay.Components;
-    using Gameplay.Components.Auth;
-    using Asphalt.api.Event.player;
-    using System;
-
     [RequireComponent(typeof(CustomTextComponent))]
     public partial class WoodSignObject : WorldObject
     {
         public override InteractResult OnActRight(InteractionContext context)
         {
             PlayerInteractObjectEvent _event = new PlayerInteractObjectEvent(context);
-            Asphalt.api.AsphaltPlugin.Instance.GetEventService().CallEvent(_event);
+            EventManager.Instance.CallEvent(_event);
             if (_event.IsCancelled()) return base.OnActRight(context);
 
             return base.OnActRight(context);
