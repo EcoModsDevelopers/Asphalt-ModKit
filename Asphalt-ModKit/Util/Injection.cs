41eaa6daa650570c99beb488708d5415f02aee69
lordmampf <lordmampf@users.noreply.github.com>
2018-04-10 21:23:18 +0200

    decide at runtime if debug or release build

--- Asphalt-ModKit/Util/Injection.cs
+++ Asphalt-ModKit/Util/Injection.cs
@@ -1,6 +1,8 @@
-﻿using Eco.Gameplay.Stats.ConcretePlayerActions;
+﻿using Eco.Gameplay.Players;
+using Eco.Gameplay.Stats.ConcretePlayerActions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -14,6 +16,26 @@ namespace Asphalt.Api.Util
     {
         private const BindingFlags PUBLIC_STATC = BindingFlags.Static | BindingFlags.Public;
         private const BindingFlags PUBLIC_INSTANCE = BindingFlags.Instance | BindingFlags.Public;
+
+        private static readonly bool isCompiledInDebug;
+
+        static Injection()
+        {
+            isCompiledInDebug = IsAssemblyDebugBuild(typeof(User).Assembly);
+        }
+
+        private static bool IsAssemblyDebugBuild(Assembly assembly)
+        {
+            foreach (var attribute in assembly.GetCustomAttributes(false))
+            {
+                var debuggableAttribute = attribute as DebuggableAttribute;
+                if (debuggableAttribute != null)
+                {
+                    return debuggableAttribute.IsJITTrackingEnabled;
+                }
+            }
+            return false;
+        }
 
         public static void InstallCreateAtomicAction(Type pTypeToReplace, Type pHelperType)
         {
@@ -52,77 +74,77 @@ namespace Asphalt.Api.Util
                 {
                     int* inj = (int*)pMethodToInject.MethodHandle.Value.ToPointer() + 2;
                     int* tar = (int*)pMethodToReplace.MethodHandle.Value.ToPointer() + 2;
-
-#if DEBUG
-                    //             Console.WriteLine("\nVersion x86 Debug\n");
-
-                    byte* injInst = (byte*)*inj;
-                    byte* tarInst = (byte*)*tar;
-
-                    int* injSrc = (int*)(injInst + 1);
-                    int* tarSrc = (int*)(tarInst + 1);
-
-                    if (pNewLocationForMethodToReplace != null)
+                    
+                    if (isCompiledInDebug)
                     {
-                        int* newloc = (int*)pNewLocationForMethodToReplace.MethodHandle.Value.ToPointer() + 2;
-                        byte* newLocInst = (byte*)*newloc;
-                        int* newLocSrc = (int*)(newLocInst + 1);
+                        //             Console.WriteLine("\nVersion x86 Debug\n");
 
-                        *newLocSrc = (((int)tarInst + 5) + *tarSrc) - ((int)newLocInst + 5);
+                        byte* injInst = (byte*)*inj;
+                        byte* tarInst = (byte*)*tar;
+
+                        int* injSrc = (int*)(injInst + 1);
+                        int* tarSrc = (int*)(tarInst + 1);
+
+                        if (pNewLocationForMethodToReplace != null)
+                        {
+                            int* newloc = (int*)pNewLocationForMethodToReplace.MethodHandle.Value.ToPointer() + 2;
+                            byte* newLocInst = (byte*)*newloc;
+                            int* newLocSrc = (int*)(newLocInst + 1);
+
+                            *newLocSrc = (((int)tarInst + 5) + *tarSrc) - ((int)newLocInst + 5);
+                        }
+
+                        *tarSrc = (((int)injInst + 5) + *injSrc) - ((int)tarInst + 5);
                     }
-
-                    *tarSrc = (((int)injInst + 5) + *injSrc) - ((int)tarInst + 5);
-#else
-                    //    Console.WriteLine("\nVersion x86 Release\n");
-
-                    if (pNewLocationForMethodToReplace != null)
+                    else
                     {
-                        int* newloc = (int*)pNewLocationForMethodToReplace.MethodHandle.Value.ToPointer() + 2;
-                        *newloc = *tar;
-                    }
+                        //    Console.WriteLine("\nVersion x86 Release\n");
 
-                    *tar = *inj;
-#endif
+                        if (pNewLocationForMethodToReplace != null)
+                        {
+                            int* newloc = (int*)pNewLocationForMethodToReplace.MethodHandle.Value.ToPointer() + 2;
+                            *newloc = *tar;
+                        }
+
+                        *tar = *inj;                    }
                 }
                 else
                 {
 
                     long* inj = (long*)pMethodToInject.MethodHandle.Value.ToPointer() + 1;
                     long* tar = (long*)pMethodToReplace.MethodHandle.Value.ToPointer() + 1;
-#if DEBUG
-                    //           Console.WriteLine("\nVersion x64 Debug\n");
-                    byte* injInst = (byte*)*inj;
-                    byte* tarInst = (byte*)*tar;
-
-
-                    int* injSrc = (int*)(injInst + 1);
-                    int* tarSrc = (int*)(tarInst + 1);
-
-
-                    if (pNewLocationForMethodToReplace != null)
+                    if (isCompiledInDebug)
                     {
-                        long* newloc = (long*)pNewLocationForMethodToReplace.MethodHandle.Value.ToPointer() + 1;
-                        byte* newLocInst = (byte*)*newloc;
-                        int* newLocSrc = (int*)(newLocInst + 1);
+                        //           Console.WriteLine("\nVersion x64 Debug\n");
+                        byte* injInst = (byte*)*inj;
+                        byte* tarInst = (byte*)*tar;
+                        
+                        int* injSrc = (int*)(injInst + 1);
+                        int* tarSrc = (int*)(tarInst + 1);
 
-                        *newLocSrc = (((int)tarInst + 5) + *tarSrc) - ((int)newLocInst + 5);
+                        if (pNewLocationForMethodToReplace != null)
+                        {
+                            long* newloc = (long*)pNewLocationForMethodToReplace.MethodHandle.Value.ToPointer() + 1;
+                            byte* newLocInst = (byte*)*newloc;
+                            int* newLocSrc = (int*)(newLocInst + 1);
+
+                            *newLocSrc = (((int)tarInst + 5) + *tarSrc) - ((int)newLocInst + 5);
+                        }
+
+                        *tarSrc = (((int)injInst + 5) + *injSrc) - ((int)tarInst + 5);
                     }
-
-                    *tarSrc = (((int)injInst + 5) + *injSrc) - ((int)tarInst + 5);
-#else
-
-                    //        Console.WriteLine("\nVersion x64 Release\n");
-
-                    if (pNewLocationForMethodToReplace != null)
+                    else
                     {
-                        long* newloc = (long*)pNewLocationForMethodToReplace.MethodHandle.Value.ToPointer() + 1;
-                        *newloc = *tar;
+                        //        Console.WriteLine("\nVersion x64 Release\n");
+
+                        if (pNewLocationForMethodToReplace != null)
+                        {
+                            long* newloc = (long*)pNewLocationForMethodToReplace.MethodHandle.Value.ToPointer() + 1;
+                            *newloc = *tar;
+                        }
+
+                        *tar = *inj;
                     }
-
-                    *tar = *inj;
-
-#endif
-
                 }
             }
         }
