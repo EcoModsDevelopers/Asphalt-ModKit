9a0fbe8719f95abe764776a102978908f5ddf2c5
lordmampf <lordmampf@users.noreply.github.com>
2018-04-24 22:06:06 +0200

    Some utils: Needs finishing programming

---
+++ Asphalt-ModKit/Util/SkillsUtil.cs
@@ -0,0 +1,34 @@
+﻿using Eco.Gameplay.Players;
+using Eco.Gameplay.Skills;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asphalt.Util
+{
+    public static class SkillsUtil
+    {
+        public static bool HasSkillLevel(User user, Type skillType, int level)
+        {
+            Skill[] skills = user.Skillset.Skills;
+            return skills.Any(s => s.Type == skillType && s.Level >= level);
+        }
+
+        public static int GetSkillLevel(User user, Type skillType)
+        {
+            Skill[] skills = user.Skillset.Skills;
+
+            foreach (Skill skill in skills)
+            {
+                if (skill.Type == skillType)
+                {
+                    return skill.Level;
+                }
+            }
+            return 0;
+        }
+    }
+
+}
