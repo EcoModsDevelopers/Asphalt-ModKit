f2707d22b06af1a3e13767d3488c5fb24ec0b236
lordmampf <lordmampf@users.noreply.github.com>
2018-09-13 18:10:48 +0200

    update to 0.7.7.2

--- Asphalt-ModKit-ExamplePlugin/EcoTestEventPlugin.cs
+++ Asphalt-ModKit-ExamplePlugin/EcoTestEventPlugin.cs
@@ -1,20 +1,4 @@
 ﻿using Eco.Core.Plugins.Interfaces;
-using Eco.Gameplay.Stats;
-using Eco.Gameplay.Stats.ConcretePlayerActions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Eco.Core.Utils.AtomicAction;
-using Eco.Core.Utils;
-using Eco.Shared.Localization;
-using System.Reflection;
-using Eco.Stats;
-using Eco.Gameplay.Interactions;
-using Eco.Shared.Items;
-using Eco.Gameplay.Players;
-using Eco.World;
 using Asphalt.Api.Event;
 
 namespace EcoTestEventPlugin
