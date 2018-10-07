7f0c29427df93718422c9769ce0472cb38ad8292
ayan4m1 <andrew@bulletlogic.com>
2018-10-07 12:17:23 -0400

    add WorldObjectPickupEvent

---
+++ Asphalt-ModKit/Events/WorldObjectEvent/WorldObjectPickupEvent.cs
@@ -0,0 +1,39 @@
+﻿using Asphalt.Api.Event;
+using Eco.Core.Utils.AtomicAction;
+using Eco.Gameplay.Objects;
+using Eco.Gameplay.Players;
+using Eco.Shared.Localization;
+
+namespace Asphalt.Events.WorldObjectEvent
+{
+    public class WorldObjectPickupEvent : CancellableEvent
+    {
+        public WorldObject WorldObject { get; set; }
+        public Player Picker { get; set; }
+
+        public WorldObjectPickupEvent(ref WorldObject worldObject, ref Player picker) : base()
+        {
+            WorldObject = worldObject;
+            Picker = picker;
+        }
+    }
+
+    internal class WorldObjectPickupEventHelper
+    {
+        public static bool Prefix(ref Player player, ref WorldObject __instance, ref IAtomicAction __result)
+        {
+            var wope = new WorldObjectPickupEvent(ref __instance, ref player);
+            var wopeEvent = (IEvent)wope;
+
+            EventManager.CallEvent(ref wopeEvent);
+
+            if (wope.IsCancelled())
+            {
+                __result = new FailedAtomicAction(new LocString("Asphalt " + nameof(WorldObjectPickupEvent)));
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
