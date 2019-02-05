077c46e81e6a5359997dbfc436008057d31627d4
lordmampf <lordmampf@users.noreply.github.com>
2019-02-05 20:40:34 +0100

    set version to 0.5.3.0

---
+++ Asphalt-ModKit/Util/AsphaltLog.cs
@@ -0,0 +1,43 @@
+﻿using Eco.Shared.Localization;
+using Eco.Shared.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asphalt.Util
+{
+    public static class AsphaltLog
+    {
+        public static void Write(string message)
+        {
+            Log.Write(new LocString(message));
+        }
+
+        public static void WriteError(string message)
+        {
+            Log.WriteError(new LocString(message));
+        }
+
+        public static void WriteError(string message, Exception ex)
+        {
+            Log.WriteError(new LocString(message), ex);
+        }
+
+        public static void WriteErrorLine(string message)
+        {
+            Log.WriteErrorLine(new LocString(message));
+        }
+
+        public static void WriteException(Exception ex)
+        {
+            Log.WriteException(ex);
+        }
+
+        public static void WriteLine(string message)
+        {
+            Log.WriteLine(new LocString(message));
+        }
+    }
+}
---
+++ Asphalt-ModKit/Util/PlayerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Eco.Gameplay.Players;
+using Eco.Gameplay.Systems.Chat;
+using Eco.Shared.Localization;
+using Eco.Shared.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asphalt.Util
+{
+    public static class PlayerExtensions
+    {
+        public static void SendTemporaryMessage(this Player pPlayer, string message, ChatCategory category = ChatCategory.Info)
+        {
+            pPlayer.SendTemporaryMessage(new LocString(message), category);
+        }
+
+        public static void SendTemporaryError(this Player pPlayer, string message, ChatCategory category = ChatCategory.Info)
+        {
+            pPlayer.SendTemporaryError(new LocString(message), category);
+        }
+
+        public static void SendMessage(this User pUser, string pMessage, bool temporary = true, DefaultChatTags tag = DefaultChatTags.Notifications, ChatCategory category = ChatCategory.Info)
+        {
+            ChatManager.ServerMessageToPlayer(new LocString(pMessage), pUser, temporary, tag, category);
+        }
+    }
+}
